a917cb49f93a6a49b1aa36d156e7788e96308bca
ronaldof16 <ronaldof16@hotmail.com>
2023-01-20 16:52:50 -0300

    Resolução do exercício 10

---
+++ Curso Matheus Battisti/exercício10/ex10.js
@@ -0,0 +1,7 @@
+let velocidade = 85;
+
+if(velocidade <= 80) {
+    console.log("Você está abaixo da velocidade permitida, parabéns!");
+}  else {
+    console.log("Você está acima da velocidade permitida, cuidado!");
+}
