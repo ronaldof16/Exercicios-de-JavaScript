ff19a7b635022ceaa82333fa96fb4dc78463d6fa
ronaldof16 <ronaldof16@hotmail.com>
2023-01-21 10:54:00 -0300

    Resolução do exercício 14

---
+++ Curso Matheus Battisti/exercício14/ex14.js
@@ -0,0 +1,19 @@
+for(let i = 0;i <= 50; i++) {
+    if(i % 2 == 0) {
+        console.log(i + ' é par!');
+    } else {
+        console.log(i + ' é ímpar!');
+    }
+}
+
+let x = 0;
+
+while(x <= 50) {
+    if(x % 2 == 0) {
+        console.log(x + ' É um número par!');
+    } else {
+        console.log(x + ' É um  número ímpar!');
+    }
+
+    x++;
+}
