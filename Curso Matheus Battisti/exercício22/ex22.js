eee128523a5ce103afc8ebe6c533c7a99983d63c
ronaldof16 <ronaldof16@hotmail.com>
2023-01-24 09:57:31 -0300

    Resolução do exrcício 22

---
+++ Curso Matheus Battisti/exercício22/ex22.js
@@ -0,0 +1,12 @@
+function valorAbsoluto1(x) {
+    console.log(Math.abs(x));
+}
+
+valorAbsoluto1(-250);
+
+
+function valorAbsoluto2(y) {
+    return Math.abs(y);
+}
+
+console.log(valorAbsoluto2(-3));
