07ca799333feb465526b355218c79cf1b96b87d5
ronaldof16 <ronaldof16@hotmail.com>
2023-01-23 23:07:52 -0300

    Resoluçao do exercício 19

---
+++ Curso Matheus Battisti/exercício19/ex19.js
@@ -0,0 +1,7 @@
+function numeroAleatorio(x) {
+    return Math.floor(Math.random() * x) + 1;
+}
+
+console.log(numeroAleatorio(20));
+console.log(numeroAleatorio(35));
+console.log(numeroAleatorio(208));
