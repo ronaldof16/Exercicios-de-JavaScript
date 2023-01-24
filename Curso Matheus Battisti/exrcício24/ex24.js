920501865191f6031fb31ddb5959bf3198605eb3
ronaldof16 <ronaldof16@hotmail.com>
2023-01-24 10:19:55 -0300

    Resolução do exercício 24

---
+++ Curso Matheus Battisti/exrcício24/ex24.js
@@ -0,0 +1,13 @@
+function potencia(x, y) {
+    return Math.pow(x, y);
+}
+
+console.log(potencia(2, 3));
+console.log(potencia(4, 3));
+
+function potencia2(x, y) {
+    return x ** y;
+}
+
+console.log(potencia2(5, 3));
+console.log(potencia2(3, 2));
