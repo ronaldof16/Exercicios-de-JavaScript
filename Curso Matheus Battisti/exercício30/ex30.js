c99a44750389baef25d980930d6a49ebb3d338e3
ronaldof16 <ronaldof16@hotmail.com>
2023-01-26 17:00:15 -0300

    Resolução do exercício 30

---
+++ Curso Matheus Battisti/exercício30/ex30.js
@@ -0,0 +1,7 @@
+let nomes = ["Ronaldo", "Niedja", "Lucas", "Fernanado", "Negão"];
+
+if(nomes.includes("Ronaldo")) {
+    console.log("Seu nome está no array!")
+} else {
+    console.log("Seu nome não está no array!")
+}
