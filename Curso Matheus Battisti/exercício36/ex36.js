ce7eaeaca921244b2d52905032fa4418b6469eea
ronaldof16 <ronaldof16@hotmail.com>
2023-01-30 10:12:46 -0300

    Finalização do exercício 36

--- Curso Matheus Battisti/exercício36/ex36.js
+++ Curso Matheus Battisti/exercício36/ex36.js
@@ -1,15 +1,37 @@
-let Conta = {
+let conta1 = {
     saldo: 100,
-    deposito: function(valor) {
-        return valor;
+    deposito: function(x) {
+        return this.saldo + x;
     },
-    saque: function(valor) {
-        return valor;
+    saque: function(y) {
+        return this.saldo - y;
     }
 }
 
-console.log(Conta.saldo);
-console.log(Conta.deposito(20));
-console.log(Conta.saque(50));
+console.log(conta1.saldo);
+console.log(conta1.deposito(50));
+console.log(conta1.saque(30));
 
 
+// Utilizando o ES6
+
+class Conta {
+    constructor(saldo) {
+        this.saldo = saldo;
+    }
+
+    deposito(valor) {
+        this.saldo += valor;
+    }
+
+    saque(valor) {
+        this.saldo -= valor;
+    }
+}
+
+let conta = new Conta(200);
+console.log(conta.saldo);
+conta.deposito(30)
+console.log(conta.saldo);
+conta.saque(80);
+console.log(conta.saldo);
