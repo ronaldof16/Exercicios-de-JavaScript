a2482420d6e98f41051a5254aecdb923e4aa9f4c
ronaldof16 <ronaldof16@hotmail.com>
2023-01-31 13:16:00 -0300

    Resolução do exercício 39

---
+++ Curso Matheus Battisti/exercício39/ex39.js
@@ -0,0 +1,28 @@
+class Carro {
+    constructor(marca, cor, gasolinaRestante, consumo) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gasolinaRestante = gasolinaRestante;
+        this.consumo = consumo;
+    }
+
+    dirigir(km) {
+        let litrosConsumidos = km / this.consumo;
+        this.gasolinaRestante -= litrosConsumidos;
+    }
+    abastecer(l) {
+        this.gasolinaRestante += l;
+    }
+}
+
+let carro = new Carro("Fiat", "Prata", 25, 12);
+
+console.log(carro);
+carro.dirigir(50);
+console.log(carro);
+carro.abastecer(15);
+console.log(carro.gasolinaRestante);
+console.log(carro);
+
+carro.dirigir(100);
+console.log(carro);
